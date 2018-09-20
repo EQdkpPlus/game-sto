fb78a1c4615aba02b20b0e7b132ec5b60cb18851
GodMod <GodMod@users.noreply.github.com>
2018-09-20 20:13:58 +0200

    Bump version

--- sto.class.php
+++ sto.class.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
 if(!class_exists('sto')) {
 	class sto extends game_generic {
 		protected static $apiLevel	= 20;
-		public $version				= '1.1.0';
+		public $version				= '1.1.1';
 		protected $this_game		= 'sto';
 		protected $types			= array('classes', 'races');
 		protected $classes			= array();
