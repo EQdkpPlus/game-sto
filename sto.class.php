1b7de9f51611e0371bf2a0da8b7a34734467cafd
GodMod <godmod23@web.de>
2018-09-20 19:27:49 +0200

    Added some profilefields

--- sto.class.php
+++ sto.class.php
@@ -81,6 +81,141 @@ if(!class_exists('sto')) {
 		protected function load_filters($langs) {}
 
 		public function install($install=false){}
+		
+		public function profilefields(){
+			// Category 'character' is a fixed one! All others are created dynamically!
+			$this->load_type('professions', array($this->lang));
+			$this->load_type('realmlist', array($this->lang));
+			$xml_fields = array(
+					'level'	=> array(
+							'type'			=> 'spinner',
+							'category'		=> 'character',
+							'lang'			=> 'uc_level',
+							'max'			=> 50,
+							'min'			=> 1,
+							'undeletable'	=> false,
+							'sort'			=> 4
+					),
+					'ruf1'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_1',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf2'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_2',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf3'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_3',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf4'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_4',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf5'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_5',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf6'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_6',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf7'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_7',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf8'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_8',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf9'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_9',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf10'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_10',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf11'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_11',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf12'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_12',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+					'ruf13'	=> array(
+							'type'			=> 'dropdown',
+							'category'		=> 'uc_ruf',
+							'lang'			=> 'uc_ruf_13',
+							'options'		=> array(''=>'', 'I' => 'I', 'II' => 'II', 'III' => 'III', 'IV' => 'IV', 'V' => 'V'),
+							'tolang'		=> true,
+							'undeletable'	=> false,
+							'sort'			=> 1
+					),
+			);
+			return $xml_fields;
+		}
 	}
 }
 ?>
